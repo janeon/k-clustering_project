d76463b59dff7ed641be4d25bca9b5e49df51206
janeon <JaneHsieh@ip-75-95.wireless.oberlin.edu>
2018-05-01 21:21:24 -0400

    Clustering has been debugged!

--- clustering.cpp
+++ clustering.cpp
@@ -10,67 +10,45 @@
 #include <math.h>
 using namespace std;
 
-
 class Point {
 private:
 	double x;
 	double y;
 	string name;
-	Point ClustCenter;
-
-public:
+	Point *ClustCenter;
+    
+public: // The class for a point and their methods
 	Point(double x, double y, string name) {
 		this->x = x;
 		this->y = y;
 		this->name = name;
 	}
-
 	double distToPoint(Point dest) {
-        double a = sqrt((this->x*this->x)+(this->y*this->y));
-        double b = sqrt((dest->x*dest->x)+(dest->y*dest->y));
+        double a = sqrt((this->x * this->x)+(this->y * this->y));
+        double b = sqrt((dest.x * dest.x)+(dest.y * dest.y));
         return (sqrt(a*a+b*b));
 		// calculate the distance between this point and dest, based on the pythagorean theorem
 	}
-
-	double getx() {
-
-		return this->x;
-	}
-
-	double gety() {
-
-		return this->y;
-	}
-
-	Point getClustCenter() {
-
-		return this->ClustCenter;
-	}
-
-	void setClustCenter(Point newCenter) {
-
-		this->ClustCenter = newCenter;
-	}
+    double getx() { return this->x; }
+    double gety() { return this->y; }
+    Point getClustCenter() { return *(this->ClustCenter);}
+    void setClustCenter(Point newCenter) { this->ClustCenter = &newCenter;}
 };
-
 
 class Cluster {
 private:
-	Point center;
+	Point *center;
 	int numPoints;
-
 public:
-	Cluster(Point center) {
-		this->center = center;
+	Cluster(Point *ctr) {
+		this->center = ctr;
 	}
 
 	Point getClustCenter() {
-
-		return this->center;
+		return *(this->center);
 	}
 
 	int getNumPoints() {
-
 		return this->numPoints;
 	}
 };
@@ -79,24 +57,21 @@ public:
 class Map {
 private:
 	int totPoints;
-	Point allPoints[];
+	Point *allPoints[0];
 	int allCenters[];
 
 public:
 
 	Map() {
-
 		totPoints = 0;
 	}
 
 	int getTotPoints() {
-
 		return this->totPoints;
 	}
 
 	Point * getAllPoints() {
-
-		return this->allPoints;
+		return *(this->allPoints);
 	}
 
 	int * getAllCenters() {
@@ -115,7 +90,7 @@ public:
 	}
 
 	double getDist(Point a, Point b) {
-
+        return a.distToPoint(b);
 		// returns the distance between 2 points
 	}
 };
@@ -125,8 +100,8 @@ class KM {
 private:
 	int k; // specified number of clusters
 	int limit; // the iteration limit of the algorithm
-	Point oldCenters[]; // stores the previously calculated centers for comparison
-
+	Point oldCenters[0]; // stores the previously calculated centers for comparison
+    int changed;
 	void randCenters(int k) {
 
 		// initializes the map so that random k points are set as initial clusters
@@ -147,12 +122,12 @@ public:
 	}
 
 	Point * reCenter() {
-
+        return NULL;
 		// calculate new centers based on average coordinates of all points in each cluster
 	}
 
 	int ifChanged() {
-
+        return changed; // remember to update changed as recenter is done
 		// return whether or not the centers have changed by comparing oldCenters[] and map->allCenters[]
 	}
 };
@@ -161,6 +136,9 @@ public:
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    Point *origin  = new Point(1,2,"origin");
+    Point *pt  = new Point(1,2,"first point");
+    cout << "The point's x-coord is" << pt->getx() << endl;
 	return 0;
 
 	// runs the algorithm
