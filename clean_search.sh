e884975f985b002d4d0a61fc03501d3417ebdc14
akidderw <35230263+akidderw@users.noreply.github.com>
2018-05-15 13:54:56 -0400

    I forgot that piping existed

--- clean_search.sh
+++ clean_search.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 #run this with the SIM search file:
 
-touch temp_galaxies.txt
-touch temp_search.txt
-grep "|G  " $1 > temp_galaxies.txt #gives us galaxies
-cut -f 2,5 -d "|" temp_galaxies.txt > temp_search.txt #gives us the names and numbers
-sed '/|No Coord./d' temp_search.txt #removes datapoints with no coordinates
 
-rm temp_galaxies.txt
-rm temp_search.txt
+grep "|G  " $1      |  #gives us galaxies
+cut -f 2,5 -d "|"   |  #gives us the names and numbers
+sed '/|No Coord./d'    #removes datapoints with no coordinates
