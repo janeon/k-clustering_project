33c44dfcb2eccacfc24f89a4885a67ba48a6443c
Aidan K <aidankw@ip-117-12.wireless.oberlin.edu>
2018-05-15 14:55:17 -0400

    Organized into files

---
+++ clustering_files/clustering.h
@@ -0,0 +1,31 @@
+/*
+ * Header file for clustering.
+ */
+
+#ifndef CLUSTERING_H
+#define CLUSTERING_H
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <tuple>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <string.h>
+
+class Point;
+class Cluster;
+class Map;
+class KMeans;
+
+
+#endif
